681569ec97cadfc74c8e78760cf18f2838108b60
Spencer O'HEGARTY <Spencer.OHEGARTY@EDUCATION.GOV.UK>
2026-06-01 17:04:54 +0100

    Initial get establishments test scaffold

---
+++ tests/DfE.EducationProviderRegistry.Core.Query.UnitTests/Establishments/Application/UseCases/GetEstablishmentsUseCaseUnitTests.cs
@@ -0,0 +1,10 @@
+namespace DfE.EducationProviderRegistry.Core.Query.UnitTests;
+
+public class EstablishmentDtoToModelMapperUnitTests
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
