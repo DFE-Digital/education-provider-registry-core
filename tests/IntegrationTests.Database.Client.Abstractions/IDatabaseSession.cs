aaa345fdf6ba20fafce519bca8d927d3d9b53045
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-06-02 09:55:28 +0100

    client abstractions for consuming factory/builders creating data without coupling to efcore

---
+++ tests/IntegrationTests.Database.Client.Abstractions/IDatabaseSession.cs
@@ -0,0 +1,9 @@
+﻿namespace IntegrationTests.Database.Client.Abstractions;
+
+public interface IDatabaseSession
+{
+    void Add<TEntity>(TEntity entity) where TEntity : class;
+    void AddRange(IEnumerable<object> entities);
+    void Update<TEntity>(TEntity entity) where TEntity : class;
+    IQueryable<TEntity> Query<TEntity>() where TEntity : class;
+}
---
+++ tests/IntegrationTests.Database.Client.Abstractions/IUnitOfWorkFactory.cs
@@ -0,0 +1,6 @@
+﻿namespace IntegrationTests.Database.Client.Abstractions;
+
+public interface IUnitOfWorkFactory
+{
+    Task<IUnitOfWork> CreateAsync(CancellationToken ctx = default);
+}
