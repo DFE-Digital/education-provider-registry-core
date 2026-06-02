9f03147fabbb95ab78e14d68be782bd8b0ca6305
aahmed-dfe <119260940+aahmed-dfe@users.noreply.github.com>
2026-06-02 10:41:49 +0100

    bring in postgres database server abstraction impl behind container (#12)

---
+++ tests/IntegrationTests.Database.Server.Postgres/Container/PostgresDatabaseOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntegrationTests.Database.Server.Postgres.Container;
+
+public sealed class PostgresDatabaseOptions
+{
+#nullable disable
+    public string Database { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+#nullable enable
+}
