f47963b06eca6059229957f2bf0ba0aeca3d9018
aahmed-dfe <119260940+aahmed-dfe@users.noreply.github.com>
2026-06-03 15:15:56 +0100

    integration-tests: Add IntegrationTests.Abstractions - BaseTests with test lifecycle hooks (#14)
    * add abstractions proj
    
    * add test abstractions and base test classes with hooks to enable use case resolve
    
    * format
    
    * add sample test to pass ci

---
+++ tests/DfE.EducationProviderRegistry.Core.Query.IntegrationTests/UseCaseIntegrationTestBase.cs
@@ -0,0 +1,48 @@
+﻿using IntegrationTests.Abstractions;
+using IntegrationTests.Database.Server.Abstractions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DfE.EducationProviderRegistry.Core.Query.IntegrationTests;
+
+internal abstract class UseCaseIntegrationTestBase : IntegrationTestBase, IAsyncLifetime
+{
+    private readonly IDatabaseFactory _databaseFactory;
+
+    protected IDatabase? Database { get; private set; }
+
+    protected UseCaseIntegrationTestBase(IServiceProvider testServicesProvider)
+        : base(testServicesProvider)
+    {
+        _databaseFactory = TestServicesProvider.GetRequiredService<IDatabaseFactory>();
+    }
+
+    // Hook called by XUnit to initialise before any tests run
+    public async ValueTask InitializeAsync()
+    {
+        await StartTestAsync(ct: TestContext.Current.CancellationToken);
+    }
+
+    protected override async Task StartTestDependenciesAsync(CancellationToken ct = default)
+    {
+        Database = await _databaseFactory.CreateAsync(ct);
+        await Database.StartAsync(ct);
+    }
+
+    protected override Task<IConfiguration> GetApplicationConfigurationAsync()
+    {
+        // TODO options from application to set DatabaseConnection
+        return Task.FromResult<IConfiguration>(
+            DefaultConfigurationBuilder
+                .Create()
+                .Build());
+    }
+
+    protected override async Task BeforeDisposeAsync()
+    {
+        if (Database != null)
+        {
+            await Database.DisposeAsync();
+        }
+    }
+}
---
+++ tests/IntegrationTests.Abstractions/DefaultConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace IntegrationTests.Abstractions;
+
+public static class DefaultConfiguration
+{
+    public static IConfiguration Create() => DefaultConfigurationBuilder.Create().Build();
+}
