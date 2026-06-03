f16106ee87137c5d44b8fbd4d3a3ddb814ce4265
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-06-03 16:25:29 +0100

    add impl for getestablishments happy path, wrap runscopedasync for usecases

---
+++ tests/DfE.EducationProviderRegistry.Core.Query.IntegrationTests/Tests/GetEstablishments/GetEstablishmentServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using DfE.EducationProviderRegistry.Core.Query.Establishments;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DfE.EducationProviderRegistry.Core.Query.IntegrationTests.Tests.GetEstablishments;
+
+internal static class GetEstablishmentServiceCollectionExtensions
+{
+    internal static IServiceCollection AddGetEstablishments(this IServiceCollection services)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        services
+            .AddEstablishmentsUseCaseDependencies()
+            .AddEstablishmentsInfrastructureDependencies();
+
+        return services;
+    }
+}
