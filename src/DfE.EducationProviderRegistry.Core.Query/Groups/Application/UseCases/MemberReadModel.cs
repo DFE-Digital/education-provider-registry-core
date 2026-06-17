cc09708453f69318b27d020a8883e9446a634b86
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-06-17 22:24:49 +0100

    add member to readmodel mapper and tests

---
+++ src/DfE.EducationProviderRegistry.Core.Query/Groups/Application/UseCases/MemberReadModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DfE.EducationProviderRegistry.Core.Query.Groups.Application.UseCases;
+
+public sealed record MemberReadModel
+{
+    public required string Identifier { get; init; }
+    public required string FullName { get; init; }
+    public required DateTime StartDate { get; init; }
+}
