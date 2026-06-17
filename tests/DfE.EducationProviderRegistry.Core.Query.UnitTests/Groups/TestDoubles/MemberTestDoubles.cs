7071fa87adcbd42aedaddae0a361c82baa500703
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-06-17 20:09:20 +0100

    fix membertestdoubles creation for group tests

--- tests/DfE.EducationProviderRegistry.Core.Query.UnitTests/Groups/TestDoubles/MemberTestDoubles.cs
+++ tests/DfE.EducationProviderRegistry.Core.Query.UnitTests/Groups/TestDoubles/MemberTestDoubles.cs
@@ -15,7 +15,7 @@ internal static class MemberTestDoubles
         for (int i = 0; i < count; i++)
         {
             members.Add(CreateWith(
-                id: faker.Random.Guid().ToString(),
+                id: faker.Random.Number(1000000, 7777777).ToString(),
                 name: faker.Person.FullName,
                 startDate: faker.Date.Past()));
         }
@@ -24,7 +24,7 @@ internal static class MemberTestDoubles
     }
 
     public static Member CreateWith(
-        string id = "member-1",
+        string id = "1234567",
         string name = "Test Member",
         DateTime? startDate = null)
     {
