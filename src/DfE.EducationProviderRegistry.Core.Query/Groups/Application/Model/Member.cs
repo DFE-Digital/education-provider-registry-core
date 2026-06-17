8fde2bb8a8b79837e7ba713e58502eac1e1682e9
aahmed-dfe <119260940+aahmed-dfe@users.noreply.github.com>
2026-06-17 20:03:48 +0100

    [feat]: add Member model for GetGroupById (#42)
    * add member model and supporting test doubles and tests
    
    * fix test double creation

---
+++ src/DfE.EducationProviderRegistry.Core.Query/Groups/Application/Model/Member.cs
@@ -0,0 +1,20 @@
+﻿using DfE.EducationProviderRegistry.Core.Query.Shared;
+
+namespace DfE.EducationProviderRegistry.Core.Query.Groups.Application.Model;
+
+public sealed record Member
+{
+    public Member(GovernanceIdentifier id, Name name, DateTime startDate)
+    {
+        ArgumentNullException.ThrowIfNull(id);
+        ArgumentNullException.ThrowIfNull(name);
+
+        Id = id;
+        Name = name;
+        StartDate = startDate;
+    }
+
+    public GovernanceIdentifier Id { get; }
+    public Name Name { get; }
+    public DateTime StartDate { get; }
+}
