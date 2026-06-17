a8e31bdfb4f3cf63f4a905fa04928522206a5839
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-06-17 20:37:09 +0100

    add groupuid

---
+++ tests/DfE.EducationProviderRegistry.Core.Query.UnitTests/Groups/TestDoubles/GroupUIDTestDoubles.cs
@@ -0,0 +1,13 @@
+﻿using DfE.EducationProviderRegistry.Core.Query.Groups.Application.Model;
+
+namespace DfE.EducationProviderRegistry.Core.Query.UnitTests.Groups.TestDoubles;
+
+internal static class GroupUIDTestDoubles
+{
+    public static GroupUID Create() => Create(1);
+
+    public static GroupUID Create(int value)
+    {
+        return new GroupUID(value);
+    }
+}
