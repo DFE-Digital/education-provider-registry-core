9bf999faf7bf7220bcaba4a95b0836ef55d3805f
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-06-17 21:28:09 +0100

    bring in trustee model

---
+++ src/DfE.EducationProviderRegistry.Core.Query/Groups/Application/Model/Trustee.cs
@@ -0,0 +1,22 @@
+﻿using DfE.EducationProviderRegistry.Core.Query.Shared;
+
+namespace DfE.EducationProviderRegistry.Core.Query.Groups.Application.Model;
+
+
+public sealed record Trustee
+{
+    public Trustee(GovernanceIdentifier id, Name name, DateTime startDate, TrusteeTitle? title = null)
+    {
+        ArgumentNullException.ThrowIfNull(id);
+        ArgumentNullException.ThrowIfNull(name);
+
+        Id = id;
+        Name = name;
+        StartDate = startDate;
+        Title = title;
+    }
+    public GovernanceIdentifier Id { get; }
+    public TrusteeTitle? Title { get; }
+    public Name Name { get; }
+    public DateTime StartDate { get; }
+}
