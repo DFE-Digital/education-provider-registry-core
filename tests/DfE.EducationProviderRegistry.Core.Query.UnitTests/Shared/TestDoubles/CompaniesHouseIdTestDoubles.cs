6514c012d170c4fdd8ffeaab65239ba253e46a0e
aahmed-dfe <119260940+aahmed-dfe@users.noreply.github.com>
2026-06-17 20:30:44 +0100

    [feat] Add CompaniesHouseId for GetGroupsById (#43)
    * add companies house id
    
    * pull in testdoubles
    
    * extend to new line chars

---
+++ tests/DfE.EducationProviderRegistry.Core.Query.UnitTests/Shared/TestDoubles/CompaniesHouseIdTestDoubles.cs
@@ -0,0 +1,13 @@
+﻿using DfE.EducationProviderRegistry.Core.Query.Groups.Application.Model;
+
+namespace DfE.EducationProviderRegistry.Core.Query.UnitTests.Shared.TestDoubles;
+
+internal static class CompaniesHouseIdTestDoubles
+{
+    public static CompaniesHouseId Create() => Create("CH123");
+
+    public static CompaniesHouseId Create(string value)
+    {
+        return new CompaniesHouseId(value);
+    }
+}
