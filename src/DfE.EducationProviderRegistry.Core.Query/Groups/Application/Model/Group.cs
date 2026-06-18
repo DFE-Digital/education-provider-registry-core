c56f806f6c3774e7c91830a696d341699a210772
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-06-18 08:20:00 +0100

    add group model and tests

---
+++ src/DfE.EducationProviderRegistry.Core.Query/Groups/Application/Model/Group.cs
@@ -0,0 +1,71 @@
+﻿namespace DfE.EducationProviderRegistry.Core.Query.Groups.Application.Model;
+
+public sealed record Group
+{
+    public Group(
+        GroupId id,
+        GroupUID uid,
+        CompaniesHouseId companiesHouseId,
+        IEnumerable<Academy> academies,
+        IEnumerable<Member>? members,
+        IEnumerable<Trustee>? trustees)
+    {
+        ArgumentNullException.ThrowIfNull(id);
+        ArgumentNullException.ThrowIfNull(companiesHouseId);
+
+        GroupId = id;
+        GroupUID = uid;
+        CompaniesHouseId = companiesHouseId;
+
+        Academies = academies?.ToList() ?? [];
+        Members = members?.ToList() ?? [];
+        Trustees = trustees?.ToList() ?? [];
+    }
+    public GroupId GroupId { get; }
+    public GroupUID GroupUID { get; }
+    public CompaniesHouseId CompaniesHouseId { get; }
+    public IReadOnlyCollection<Academy> Academies { get; }
+    public IReadOnlyCollection<Member> Members { get; }
+    public IReadOnlyCollection<Trustee> Trustees { get; }
+
+
+    public bool Equals(Group? other)
+    {
+        if (other is null) return false;
+
+        return GroupId == other.GroupId
+            && GroupUID == other.GroupUID
+            && CompaniesHouseId == other.CompaniesHouseId
+            && Academies.SequenceEqual(other.Academies)
+            && Members.SequenceEqual(other.Members)
+            && Trustees.SequenceEqual(other.Trustees);
+    }
+
+
+    public override int GetHashCode()
+    {
+        HashCode hash = new();
+        hash.Add(GroupId);
+        hash.Add(GroupUID);
+        hash.Add(CompaniesHouseId);
+
+        foreach (Academy academy in Academies)
+        {
+            hash.Add(academy);
+        }
+
+        foreach (Member member in Members)
+        {
+            hash.Add(member);
+        }
+
+
+        foreach (Trustee trustee in Trustees)
+        {
+            hash.Add(trustee);
+        }
+
+        return hash.ToHashCode();
+    }
+
+}
