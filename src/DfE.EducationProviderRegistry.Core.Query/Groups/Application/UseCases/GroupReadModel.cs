004a4d6b4019af1fa6bb3c6a00da02dc4582cccd
aahmed-dfe <119260940+aahmed-dfe@users.noreply.github.com>
2026-06-18 08:56:19 +0100

    add group read model and mapper (#50)

---
+++ src/DfE.EducationProviderRegistry.Core.Query/Groups/Application/UseCases/GroupReadModel.cs
@@ -0,0 +1,13 @@
+﻿using DfE.EducationProviderRegistry.Core.Query.Groups.Application.Model;
+
+namespace DfE.EducationProviderRegistry.Core.Query.Groups.Application.UseCases;
+
+public sealed record GroupReadModel
+{
+    public required string GroupId { get; init; }
+    public required int GroupUID { get; init; }
+    public required string CompaniesHouseId { get; init; }
+    public required IReadOnlyCollection<Academy> Academies { get; init; }
+    public required IReadOnlyCollection<MemberReadModel> Members { get; init; }
+    public required IReadOnlyCollection<TrusteeReadModel> Trustees { get; init; }
+}
---
+++ src/DfE.EducationProviderRegistry.Core.Query/Groups/Application/UseCases/GroupToGroupReadModelMapper.cs
@@ -0,0 +1,35 @@
+﻿using DfE.Core.Libraries.CrossCutting.Mapper;
+using DfE.EducationProviderRegistry.Core.Query.Groups.Application.Model;
+
+namespace DfE.EducationProviderRegistry.Core.Query.Groups.Application.UseCases;
+
+internal sealed class GroupToGroupReadModelMapper : IMapper<Group, GroupReadModel>
+{
+    private readonly IMapper<IEnumerable<Trustee>, IReadOnlyCollection<TrusteeReadModel>> _trusteeToDtoMapper;
+    private readonly IMapper<IEnumerable<Member>, IReadOnlyCollection<MemberReadModel>> _memberToReadModelMapper;
+
+    public GroupToGroupReadModelMapper(
+        IMapper<IEnumerable<Member>, IReadOnlyCollection<MemberReadModel>> memberToDtoMapper,
+        IMapper<IEnumerable<Trustee>, IReadOnlyCollection<TrusteeReadModel>> trusteeToDtoMapper)
+    {
+        ArgumentNullException.ThrowIfNull(memberToDtoMapper);
+        ArgumentNullException.ThrowIfNull(trusteeToDtoMapper);
+        _trusteeToDtoMapper = trusteeToDtoMapper;
+        _memberToReadModelMapper = memberToDtoMapper;
+    }
+
+    public GroupReadModel Map(Group input)
+    {
+        ArgumentNullException.ThrowIfNull(input);
+
+        return new GroupReadModel
+        {
+            GroupId = input.GroupId.Value,
+            GroupUID = input.GroupUID.Value,
+            CompaniesHouseId = input.CompaniesHouseId.Value,
+            Academies = input.Academies.OrderBy(t => t.Name.ToString()).ToArray(),
+            Members = _memberToReadModelMapper.Map(input.Members),
+            Trustees = _trusteeToDtoMapper.Map(input.Trustees)
+        };
+    }
+}
