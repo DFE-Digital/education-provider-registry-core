49159c613c0d85237a66f46e7f1e687416bb15ff
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-06-22 11:44:41 +0100

    reduce construct args to group model

--- src/DfE.EducationProviderRegistry.Core.Query/Groups/Application/UseCases/GroupReadModel.cs
+++ src/DfE.EducationProviderRegistry.Core.Query/Groups/Application/UseCases/GroupReadModel.cs
@@ -7,7 +7,12 @@ public sealed record GroupReadModel
 {
     public required string GroupId { get; init; }
     public required int GroupUID { get; init; }
+    // public required string UKPRN { get; init; }
     public required string CompaniesHouseId { get; init; }
+    // public required GroupStatusStateReadModel Status { get; init; }
+    // public required DateTime StatusDate { get; init; }
+    // public required string Address { get; init; }
+    // public required string GroupType { get; init; }
     public required IReadOnlyCollection<Academy> Academies { get; init; }
     public required IReadOnlyCollection<MemberReadModel> Members { get; init; }
     public required IReadOnlyCollection<TrusteeReadModel> Trustees { get; init; }
