040e2255d0a978533f0baedd34aba562b109e980
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-07-01 17:17:12 +0100

    stub most of the group response, but consume dbcontext

--- src/DfE.EducationProviderRegistry.Core.Query/Groups/Application/Infrastructure/GroupData.cs
+++
@@ -1,9 +0,0 @@
-﻿using DfE.EducationProviderRegistry.Data.DatabaseModels.Models;
-
-namespace DfE.EducationProviderRegistry.Core.Query.Groups.Application.Infrastructure;
-
-internal sealed record GroupData(
-    GroupRecord Group,
-    IReadOnlyCollection<GroupIdentifier> Identifiers,
-    IReadOnlyCollection<EstablishmentGroupMembership> EstabGroupMemberships,
-    Contact? Contact);
--- src/DfE.EducationProviderRegistry.Core.Query/Groups/Application/Infrastructure/GroupRecordToGroupMapper.cs
+++ src/DfE.EducationProviderRegistry.Core.Query/Groups/Application/Infrastructure/GroupRecordToGroupMapper.cs
@@ -13,63 +13,24 @@ internal sealed class GroupRecordToGroupMapper : IMapper<GroupRecord, Group>
         ArgumentNullException.ThrowIfNull(input);
 
         GroupIdentity identity = new(
-            id: new GroupId(
-                input.GroupIdentifier
-                    .Where(i => i.IdentifierType == "GROUPID")
-                    .Select(i => i.IdentifierValue)
-                    .SingleOrDefault()!
-            ),
-            uid: new(input.GroupId)
-        );
+            id: new GroupId("STUB-GROUPID"),
+            uid: new(input.GroupId));
 
         GroupExternalIdentifiers externalIds = new(
-            ukprn: new Ukprn(
-                input.GroupIdentifier
-                    .Where(i => i.IdentifierType == "UKPRN")
-                    .Select(i => i.IdentifierValue)
-                    .SingleOrDefault()!
-            ),
-            companiesHouseId: input.GroupIdentifier
-                .Where((groupIdentifier) => groupIdentifier.IdentifierType == "COMPANIESHOUSE")
-                .Select((companiesHouseIdentifier) => companiesHouseIdentifier.IdentifierValue)
-                .SingleOrDefault() is string chn
-                    ? new CompaniesHouseId(chn)
-                    : null
-        );
+          ukprn: new Ukprn("STUB-UKPRN"),
+          companiesHouseId: new CompaniesHouseId("STUB-COMPANIESHOUSE-NUMBER"));
 
         GroupComposition composition = new(
-            academies: input.EstablishmentGroupMembership
-                .Select((estabGroupMembership) => new Academy(
-                    new AcademyId(
-                        new UniqueReferenceNumber(estabGroupMembership.Establishment.Urn)),
-                    new AcademyName(estabGroupMembership.Establishment.Name)))
-                .ToList(),
-
-            members: input.RoleAssignment
-                .Where((roleAssignment) => roleAssignment.Role.RoleType.Code == "MEMBER")
-                .Select((memberRoleAssignment) => new Member(
-                    new GovernanceIdentifier(memberRoleAssignment.RoleId.ToString()),
-                    new Name(memberRoleAssignment.Role.Person.DisplayName),
-                    startDate: DateTime.UtcNow // placeholder until schema updated
-                ))
-                .ToList(),
-
-            trustees: input.RoleAssignment
-                .Where(roleAssignment => roleAssignment.Role.RoleType.Code == "TRUSTEE")
-                .Select(trusteeRoleAssignment => new Trustee(
-                    id: null,
-                    name: new Name(trusteeRoleAssignment.Role.Person.DisplayName),
-                    startDate: DateTime.UtcNow, // placeholder
-                    title: null
-                ))
-                .ToList()
+            academies: [],
+            members: [],
+            trustees: []
         );
 
         GroupCharacteristics characteristics = new(
             name: new Name(input.Name),
-            address: new Address("123 Test Street", "Testville", "Testshire", "TE5 5ST"),   // placeholder - not yet supported in schema
+            address: new Address("123 Test Street", "Testville", "Testshire", "TE5 5ST"), // STUB
             type: new GroupType(input.GroupType.Name),
-            status: new GroupStatus(GroupOpenState.Open, new(2026, 01, 01))     // placeholder - not yet supported in schema
+            status: new GroupStatus(GroupOpenState.Open, new(2026, 01, 01)) // STUB
         );
 
         return new Group(identity, externalIds, composition, characteristics);
