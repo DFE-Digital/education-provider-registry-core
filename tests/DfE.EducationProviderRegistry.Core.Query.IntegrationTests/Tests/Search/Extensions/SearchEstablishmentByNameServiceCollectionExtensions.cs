39a5badbb1d7ef2339c3fc57eb30ed9ac219edc2
aahmed-dfe <119260940+aahmed-dfe@users.noreply.github.com>
2026-07-13 10:39:05 +0100

    add shared features extension to register crosscutting dependencies, copy sql files for now until postgres image available with mounted in (#96)

---
+++ tests/DfE.EducationProviderRegistry.Core.Query.IntegrationTests/Tests/Search/Extensions/SearchEstablishmentByNameServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using DfE.EducationProviderRegistry.Core.Query.Search;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DfE.EducationProviderRegistry.Core.Query.IntegrationTests.Tests.Search.Extensions;
+
+internal static class SearchEstablishmentByNameServiceCollectionExtensions
+{
+    internal static IServiceCollection AddSearch(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSharedFeatureDependencies(configuration);
+
+        services.AddApplicationSearchDependencies(configuration);
+        services.AddInfraSearchDependencies(configuration);
+        services.AddInfraSearchFilterDependencies(configuration);
+        return services;
+    }
+}
---
+++ tests/DfE.EducationProviderRegistry.Core.Query.IntegrationTests/Tests/SharedFeatureServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using DfE.EducationProviderRegistry.Data.DatabaseModels.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DfE.EducationProviderRegistry.Core.Query.IntegrationTests.Tests;
+
+internal static class SharedFeatureServiceCollectionExtensions
+{
+    internal static IServiceCollection AddSharedFeatureDependencies(this
+        IServiceCollection services,
+        IConfiguration configuration)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(configuration);
+
+        services
+            // TODO temporary until we have custom logger-provider set up in application-logging, COULD register IObservationHandler? on logging ITestOutputHelper / IMessageSink
+            .AddLogging();
+
+        services.AddDbContextFactory<EducationProviderRegistryDbContext>(
+            (option) =>
+                option.UseNpgsql(configuration["eprweb_eprdat_dotnet_db_connection"]));
+
+        return services;
+    }
+}
