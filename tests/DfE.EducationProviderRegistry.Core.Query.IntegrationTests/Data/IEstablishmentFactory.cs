0927fbcf47dd0b3dff9c66b3673b9743237d9ba0
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-07-15 13:32:12 +0100

    add establishment creation for future use

---
+++ tests/DfE.EducationProviderRegistry.Core.Query.IntegrationTests/Data/IEstablishmentFactory.cs
@@ -0,0 +1,10 @@
+﻿using DfE.EducationProviderRegistry.Core.Query.IntegrationTests.Data.Builders;
+using DfE.EducationProviderRegistry.Data.DatabaseModels.Models;
+
+namespace DfE.EducationProviderRegistry.Core.Query.IntegrationTests.Data;
+
+internal interface IEstablishmentFactory
+{
+    Task<Establishment> CreateAsync(
+        Action<EstablishmentBuilder>? configure = null, CancellationToken ct = default);
+}
