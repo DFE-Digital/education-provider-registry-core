3206404659ee127f8cdc28b3e3fb06edbf1196e5
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-07-15 14:13:43 +0100

    remove as merged into main

--- tests/DfE.EducationProviderRegistry.Core.Query.IntegrationTests/Data/Search/SearchableEstablishments.cs
+++
@@ -1,8 +0,0 @@
-﻿using DfE.EducationProviderRegistry.Data.DatabaseModels.Models;
-
-namespace DfE.EducationProviderRegistry.Core.Query.IntegrationTests.Data.Search;
-
-public sealed record SearchableEstablishments
-{
-    public required IReadOnlyCollection<Establishment> SearchTermMatches { get; init; }
-}
