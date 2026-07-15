bddaafd53c9e9e437e443e527c7e01e884e46794
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-07-15 12:37:45 +0100

    bring in pg stat observor

---
+++ tests/DfE.EducationProviderRegistry.Core.Query.IntegrationTests/Observer/Postgres/PostgresQueries.cs
@@ -0,0 +1,8 @@
+﻿namespace DfE.EducationProviderRegistry.Core.Query.IntegrationTests.Observer.Postgres;
+
+public sealed class PostgresQueries
+{
+    public IReadOnlyList<PostgresQuery> Queries { get; init; } = [];
+
+    public int Count => Queries.Count;
+}
---
+++ tests/DfE.EducationProviderRegistry.Core.Query.IntegrationTests/Observer/Postgres/PostgresQueryCollector.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using Npgsql;
+
+namespace DfE.EducationProviderRegistry.Core.Query.IntegrationTests.Observer.Postgres;
+
+// Maybe we can use a Clock injected to query between when GetObservationsAsync is called from StartAsync ->?
+internal sealed class PostgresQueryCollector
+    : IObservationCollector<PostgresQueries>
+{
+    private readonly string _connectionString;
+
+    public PostgresQueryCollector(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public async Task StartAsync(CancellationToken ct = default)
+    {
+        await using NpgsqlConnection conn = new(_connectionString);
+        await conn.ExecuteAsync("CREATE EXTENSION IF NOT EXISTS pg_stat_statements;");
+        await conn.ExecuteAsync("SELECT pg_stat_statements_reset();");
+    }
+
+    public Task StopAsync() => Task.CompletedTask;
+
+    public async Task<PostgresQueries> GetObservationsAsync(CancellationToken ct = default)
+    {
+        const string sql = """
+            SELECT query, calls, total_exec_time, mean_exec_time, rows 
+            FROM pg_stat_statements
+            WHERE query NOT ILIKE '%pg_stat_statements%'
+            AND query <> 'DISCARD ALL'
+        """;
+
+        await using NpgsqlConnection conn = new(_connectionString);
+
+        List<PostgresQuery> statements =
+            (await conn.QueryAsync<PostgresQuery>(sql))
+                .ToList();
+
+        return new PostgresQueries
+        {
+            Queries = statements
+        };
+    }
+
+    public ValueTask DisposeAsync() => ValueTask.CompletedTask;
+}
