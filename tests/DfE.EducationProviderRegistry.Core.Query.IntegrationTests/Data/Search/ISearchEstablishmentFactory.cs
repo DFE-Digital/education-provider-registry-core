32a0c8be8813e0b38489fa81875543bcbc3e7a73
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-07-15 14:08:43 +0100

    fix import

--- tests/DfE.EducationProviderRegistry.Core.Query.IntegrationTests/Data/Search/ISearchEstablishmentFactory.cs
+++ tests/DfE.EducationProviderRegistry.Core.Query.IntegrationTests/Data/Search/ISearchEstablishmentFactory.cs
@@ -15,17 +15,3 @@ public sealed record SearchableEstablishmentsResponse
 {
     public required IReadOnlyCollection<Establishment> SearchTermMatches { get; init; }
 }
-
-public sealed record SearchByNameTerms(IReadOnlyList<string> matchingNames)
-{
-    public static SearchByNameTerms Create(
-        string searchTerm,
-        int matchCount)
-    {
-        return new SearchByNameTerms(
-        [
-            .. Enumerable.Range(1, matchCount)
-                .Select(number => $"{searchTerm}-{number}")
-        ]);
-    }
-}
