b1c7cc8d11db3d3327ba92cb3e9879585807e4c5
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-07-21 23:50:17 +0100

    performance executes within 2s

---
+++ tests/DfE.EducationProviderRegistry.Core.Query.IntegrationTests/Tests/Search/SearchEstablishmentByNamePerformanceTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Diagnostics;
+using DfE.EducationProviderRegistry.Core.Query.IntegrationTests.Data.Search;
+using DfE.EducationProviderRegistry.Core.Query.IntegrationTests.Observer.Postgres;
+using DfE.EducationProviderRegistry.Core.Query.IntegrationTests.Tests.Search.Extensions;
+using DfE.EducationProviderRegistry.Core.Query.IntegrationTests.Tests.Search.Request;
+using DfE.EducationProviderRegistry.Core.Query.Search.Application.UseCases.Request;
+using DfE.EducationProviderRegistry.Core.Query.Search.Application.UseCases.Response;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DfE.EducationProviderRegistry.Core.Query.IntegrationTests.Tests.Search;
+
+
+public sealed class SearchEstablishmentByNamePerformanceTests : UseCaseIntegrationTestBase
+{
+    public SearchEstablishmentByNamePerformanceTests(IServiceProvider testServicesProvider) : base(testServicesProvider)
+    {
+    }
+
+    protected override void ConfigureApplicationServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddSearch(configuration);
+    }
+
+    protected override void ConfigureApplicationConfiguration(IConfigurationBuilder builder)
+    {
+        builder.AddDefaultSearchConfiguration();
+    }
+
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(50)]
+    [InlineData(5000)]
+    public async Task Executes_Within_2_Seconds(int matches)
+    {
+        // arrange
+        CancellationToken ct = TestContext.Current.CancellationToken;
+
+        const string searchTerm = "estab";
+
+        SearchableEstablishmentsResponse _ =
+            await SearchEstablishmentFactory.CreateManyAsync(
+                totalToCreate: 100_000,
+                searchTerm: searchTerm,
+                matches: SearchByNameTerms.Create(searchTerm, matchCount: matches),
+                ct);
+
+        SearchRequest request =
+            SearchRequestBuilder.Create()
+                .WithSearchKeywords(searchTerm)
+                .Build();
+
+        await QueryCollector.StartAsync(ct);
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        // act
+        UseCaseResponse<SearchResponse> response =
+            await ExecuteUseCase<SearchRequest, SearchResponse>(request);
+
+        // assert
+        stopwatch.Stop();
+        PostgresQueries observations = await QueryCollector.GetObservationsAsync(ct);
+
+        Assert.NotNull(response);
+        Assert.True(response.SuccessfulRequest);
+        Assert.NotEmpty(response.Model!.EstablishmentResults!.EstablishmentCollection);
+
+        const double expectedTotalExecutionTimeMilliseconds = 2_000;
+
+        Assert.True(
+            stopwatch.ElapsedMilliseconds < expectedTotalExecutionTimeMilliseconds,
+                $"search exceeded {expectedTotalExecutionTimeMilliseconds}ms and took {stopwatch.ElapsedMilliseconds}ms");
+
+        SearchQueryObservations searchQueryObservations = new(observations);
+        searchQueryObservations.AssertSearchPipelineExecuted(TimeSpan.FromMilliseconds(1000));
+    }
+}
