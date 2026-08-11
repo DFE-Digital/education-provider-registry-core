6252c8033769aa8b46601a850df6d131c0dc74e4
Dan Owen <daniel.owen@education.gov.uk>
2026-08-11 13:19:59 +0100

    Unit testing for property path resolution

--- tests/DfE.EducationProviderRegistry.Core.Query.UnitTests/Search/Infrastructure/QueryProcessing/Behaviours/PropertyPathResolution/ParsedPropertyPathTests.cs
+++ tests/DfE.EducationProviderRegistry.Core.Query.UnitTests/Search/Infrastructure/QueryProcessing/Behaviours/PropertyPathResolution/ParsedPropertyPathTests.cs
@@ -1,9 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using DfE.EducationProviderRegistry.Core.Query.Search.Infrastructure.QueryProcessing.Behaviours.PropertyPathResolution;
 
 namespace DfE.EducationProviderRegistry.Core.Query.UnitTests.Search.Infrastructure.QueryProcessing.Behaviours.PropertyPathResolution;
 
-internal class ParsedPropertyPathTests
+public sealed class ParsedPropertyPathTests
 {
+    [Fact]
+    public void Constructor_SetsExpectedProperties()
+    {
+        // act
+        ParsedPropertyPath result = new(
+            true,
+            "Sites",
+            "Location.Town");
+
+        // assert
+        Assert.True(result.IsCollection);
+        Assert.Equal("Sites", result.NavigationName);
+        Assert.Equal("Location.Town", result.RemainderPath);
+    }
+
+    [Fact]
+    public void Equality_SameValues_AreEqual()
+    {
+        // arrange
+        ParsedPropertyPath first = new(
+            true,
+            "Sites",
+            "Location.Town");
+
+        ParsedPropertyPath second = new(
+            true,
+            "Sites",
+            "Location.Town");
+
+        // act / assert
+        Assert.Equal(first, second);
+    }
 }
