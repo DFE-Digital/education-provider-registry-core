eb2d655538546380adba529b0e1b91013a77ee5e
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-08-24 11:34:50 +0100

    add tests for collection field

---
+++ tests/DfE.EducationProviderRegistry.Core.Query.IntegrationTests/Tests/Search/CollectionTests/SearchCollectionAndScalarFieldTests.cs
@@ -0,0 +1,74 @@
+﻿using DfE.EducationProviderRegistry.Core.Query.IntegrationTests.Data.Search;
+using DfE.EducationProviderRegistry.Core.Query.IntegrationTests.Tests.Search.Behaviours;
+using DfE.EducationProviderRegistry.Core.Query.IntegrationTests.Tests.Search.Configuration;
+using DfE.EducationProviderRegistry.Data.DatabaseModels.Models;
+
+namespace DfE.EducationProviderRegistry.Core.Query.IntegrationTests.Tests.Search.CollectionTests;
+
+public sealed class SearchCollectionAndScalarFieldTests : SearchBehaviourTestsBase
+{
+    private const string SearchTermKey = "term-1";
+
+    public SearchCollectionAndScalarFieldTests(
+        IServiceProvider testServicesProvider)
+        : base(testServicesProvider)
+    {
+    }
+
+    protected override (
+        string termKey,
+        string chainFieldsWithPredicate,
+        IEnumerable<Action<IndexedFieldConfigurationBuilder>>
+    )[] CreateSearchTermsConfiguration() =>
+    [
+        (
+            SearchTermKey,
+            IndexedFieldConfigurationBuilder.AND_CHAINING_PREDICATE,
+            [
+                builder =>
+                    builder
+                        .WithFieldName(DefaultSearchFieldName)
+                        .AppendExactMatchBehaviour(),
+
+                builder =>
+                    builder
+                        .WithFieldName(CollectionFieldName)
+                        .AppendPartialMatchBehaviour()
+            ]
+        )
+    ];
+
+    [Fact]
+    public async Task Returns_Intersection_Of_Scalar_And_Collection_Field_Matches()
+    {
+        // arrange
+        const string searchTerm = "school";
+
+        Establishment[] matchingEstablishments =
+        [
+            SearchEstablishmentBuilder.Create()
+                .SetValue(DefaultSearchFieldName, "school")
+                .WithAuthorityName("School Authority")
+                .Build()
+        ];
+
+        Establishment[] nonMatchingEstablishments =
+        [
+            SearchEstablishmentBuilder.Create()
+                .SetValue(DefaultSearchFieldName, "school")
+                .WithAuthorityName("College Authority")
+                .Build(),
+
+            SearchEstablishmentBuilder.Create()
+                .SetValue(DefaultSearchFieldName, "academy")
+                .WithAuthorityName("School Authority")
+                .Build()
+        ];
+
+        // act / assert
+        await ExecuteAndAssertSearchAsync(
+            [(SearchTermKey, searchTerm)],
+            matchingEstablishments,
+            nonMatchingEstablishments);
+    }
+}
