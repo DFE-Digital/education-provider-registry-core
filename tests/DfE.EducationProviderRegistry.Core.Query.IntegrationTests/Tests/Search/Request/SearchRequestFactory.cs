f5ad27b49ef54347c2b788a14dc6dc719a3dfc9d
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-08-24 08:25:45 +0100

    query performance tests across different index/match sizes

---
+++ tests/DfE.EducationProviderRegistry.Core.Query.IntegrationTests/Tests/Search/Request/SearchRequestFactory.cs
@@ -0,0 +1,18 @@
+﻿using DfE.EducationProviderRegistry.Core.Query.Search.Application.UseCases.Request;
+
+namespace DfE.EducationProviderRegistry.Core.Query.IntegrationTests.Tests.Search.Request;
+
+internal static class SearchRequestFactory
+{
+    internal static SearchRequest BuildSearchRequest(IEnumerable<(string key, string value)> searchTerms)
+    {
+        SearchRequestBuilder requestBuilder = SearchRequestBuilder.Create();
+
+        foreach ((string key, string value) termTuple in searchTerms)
+        {
+            requestBuilder.WithSearchTerm(termTuple);
+        }
+
+        return requestBuilder.Build();
+    }
+}
