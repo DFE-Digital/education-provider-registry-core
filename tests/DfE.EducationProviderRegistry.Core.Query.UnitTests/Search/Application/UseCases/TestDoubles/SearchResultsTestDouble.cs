10fc81360339d534c98b7bb299ec35bd3a55f7fb
aahmed-dfe <aasim.ahmed@education.gov.uk>
2026-09-16 10:58:15 +0100

    consume existing facetdouble

--- tests/DfE.EducationProviderRegistry.Core.Query.UnitTests/Search/Application/UseCases/TestDoubles/SearchResultsTestDouble.cs
+++ tests/DfE.EducationProviderRegistry.Core.Query.UnitTests/Search/Application/UseCases/TestDoubles/SearchResultsTestDouble.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using DfE.EducationProviderRegistry.Core.Query.Contracts.TestDoubles.Search;
 using DfE.EducationProviderRegistry.Core.Query.Search.Application.Models.Search;
 
 namespace DfE.EducationProviderRegistry.Core.Query.UnitTests.Search.Application.UseCases.TestDoubles;
