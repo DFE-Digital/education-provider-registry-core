a616c3c5e928a4066019d087479fae7b830ece33
aahmed-dfe <119260940+aahmed-dfe@users.noreply.github.com>
2026-09-21 08:41:44 +0100

    add providername tests (#177)

---
+++ tests/DfE.EducationProviderRegistry.Core.Query.IntegrationTests/Tests/Search/Tests/Fields/SearchUseCaseProviderNameTests.cs
@@ -0,0 +1,92 @@
+﻿using DfE.EducationProviderRegistry.Core.Query.IntegrationTests.Tests.Search.Configuration;
+using DfE.EducationProviderRegistry.Core.Query.IntegrationTests.Tests.Search.Request;
+using DfE.EducationProviderRegistry.Core.Query.Search.Application.UseCases.Request;
+using DfE.EducationProviderRegistry.Core.Query.Search.Application.UseCases.Response;
+using DfE.EducationProviderRegistry.Core.Query.Test.Database.Data.Search;
+using DfE.EducationProviderRegistry.Data.DatabaseModels.Models;
+
+namespace DfE.EducationProviderRegistry.Core.Query.IntegrationTests.Tests.Search.Tests.Fields;
+
+public sealed class SearchUseCaseProviderNameTests : SearchUseCaseMatchesResultsTestBase
+{
+    private const string SearchTermKey = "term-1";
+
+    public SearchUseCaseProviderNameTests(IServiceProvider testServicesProvider) : base(testServicesProvider)
+    {
+    }
+
+    protected override void ConfigureSearchUseCase(SearchUseCaseConfigurationBuilder builder)
+    {
+        builder.WithSearchTerm(
+            SearchTermKey,
+            (field) =>
+                field.WithFieldName(nameof(SearchAggregate.ProviderName))
+                    .AppendContainsMatchBehaviour()
+                    .AppendStartsWithMatchBehaviour());
+    }
+
+    [Fact]
+    public async Task Search_ByProvderId_Returns_Matches()
+    {
+        // Arrange
+        CancellationToken ct = TestContext.Current.CancellationToken;
+
+        IReadOnlyList<SearchAggregate> seed = [
+            SearchAggregateBuilder.Create().WithProviderName("1").Build(),
+            SearchAggregateBuilder.Create().WithProviderName("10").Build(),
+            SearchAggregateBuilder.Create().WithProviderName("2").Build(),
+        ];
+
+        await DatabaseFixture.SeedAsync<IEnumerable<SearchAggregate>, SearchableAggregates>(seed, ct);
+
+        SearchRequest request =
+            SearchRequestBuilder.Create()
+                .WithSearchTerm(SearchTermKey, "1")
+                .Build();
+
+        // Act
+        UseCaseResponse<SearchResponse> response =
+            await ExecuteAndAssertSearchAsync(
+                request,
+                expectedInResults: [seed[0], seed[1]],
+                notExpectedInResults: [seed[2]]);
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.NotNull(response.Model);
+        Assert.True(response.SuccessfulRequest);
+    }
+
+    [Fact]
+    public async Task Search_ByProviderId_No_Matches()
+    {
+        // Arrange
+        CancellationToken ct = TestContext.Current.CancellationToken;
+
+        IReadOnlyList<SearchAggregate> seed = [
+            SearchAggregateBuilder.Create().WithProviderName("1").Build(),
+            SearchAggregateBuilder.Create().WithProviderName("10").Build(),
+            SearchAggregateBuilder.Create().WithProviderName("2").Build(),
+        ];
+
+        await DatabaseFixture.SeedAsync<IEnumerable<SearchAggregate>, SearchableAggregates>(seed, ct);
+
+        SearchRequest request =
+            SearchRequestBuilder.Create()
+                .WithSearchTerm(SearchTermKey, "NOTHING")
+                .Build();
+
+        // Act
+        UseCaseResponse<SearchResponse> response =
+            await ExecuteAndAssertSearchAsync(
+                request,
+                expectedInResults: [],
+                notExpectedInResults: seed);
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.NotNull(response.Model);
+        Assert.True(response.SuccessfulRequest);
+        Assert.Empty(response.Model.SearchProviderResults!.SearchResultCollection);
+    }
+}
